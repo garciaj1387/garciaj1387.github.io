8b3211b0ab3d643e889668de47d757a92e346d9f
Julian Garcia <garciaj@outlook.com>
2018-06-14 22:17:56 -0400

    first resume website

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolorname(id){
+    document.getElementById(id).style.color= "black";
+}
+function changebackgroundcolor(id){
+    document.getElementById(id).style.backround = "light-grey";
+}
